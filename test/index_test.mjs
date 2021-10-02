cde24e308e2da4a12f9a5dc6437320bcde50d7cf
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2021-10-02 15:49:53 +0530

    URL encode code to prevent syntax errors Solves #2

--- test/index_test.mjs
+++ test/index_test.mjs
@@ -29,3 +29,26 @@ test("if errors are thrown and caught by the worker", async t => {
     async () => await run(`throw new Error("something went wrong")`)
   );
 });
+
+test('if iife can have a comment', async t => {
+  const msg = 'Printed from inside async IIFE'
+  const fn = `
+    (async () => {
+      // Prints to terminal
+      /* Logs to console */
+      console.log('${msg}')
+    })()
+  `
+  const actual = await run(fn);
+  t.is(msg, actual.toString().trim());
+})
+
+test('if es6 modules can be imported', async t => {
+  const msg = 'ES6 style import works'
+  const fn = `
+    import fs from 'fs'
+    console.log('${msg}')
+  `
+  const actual = await run(fn);
+  t.is(msg, actual.toString().trim());
+})
